7a96e9dcd8a6f4beabf8912a8f8c1e4de314582d
alicelarsson95 <alicelarsson95@hotmail.com>
2024-11-05 16:22:37 +0100

    hej

--- script.js
+++ script.js
@@ -2,13 +2,83 @@ let energyLevel = 100;
 let happinessLevel = 100;
 let hungerLevel = 100;
 
-function startTamagotchi(){
+/* let energy ;
+let happiness ;
+let hunger ; */
 
+
+const tamagotchiStates = document.querySelectorAll(".tamagotchi-img");;
+tamagotchiStates.forEach(state => {
+    state.style.display = 'none' 
+});
+
+const hatchText = document.querySelector('.hatch-text'); 
+const startButton = document.querySelector('.start-button')
+const eggImage = document.querySelector('.tamagotchi-img--egg');
+
+    startButton.addEventListener('click', function() {
+    tamagotchiStates[4].style.display = 'block';
+    hatchText.innerText = 'Hatching..';
+    eggImage.classList.add('shake');
+    setTimeout(() => {
+        eggImage.classList.remove('shake');
+    }, 5000);
+    setTimeout(startTamagotchi, 5000);
+
+    startButton.disabled = true;
+})
+
+function startTamagotchi() {
+    tamagotchiStates[4].style.display = 'none'
+    tamagotchiStates[0].style.display = 'block'
+    hatchText.innerText = "It's hatched!"
+    
+    
+    setTimeout(clearText, 2000)
+    
+    countDownLevels()
 }
 
-function feedTamagotchi(){
-
+function clearText() {
+    hatchText.innerText = ''
 }
+
+function countDownLevels() {
+ levels = setInterval(() => {
+    energyLevel -=2
+    hungerLevel -=1.5
+    happinessLevel --
+
+    if (energyLevel <= 0 || happinessLevel <= 0 || hungerLevel <= 0){
+        tamagotchiStates[3].style.display = 'none'
+        tamagotchiStates[1].style.display = 'block'
+        } else if (energyLevel <= 30 || happinessLevel <= 30 || hungerLevel <= 30) {
+          tamagotchiStates[0].style.display = 'none'
+          tamagotchiStates[3].style.display = 'block'
+        } 
+
+        if (energyLevel <= 0 || happinessLevel <= 0 || hungerLevel <= 0) {
+            clearInterval(levels)
+        }    
+        console.log('energi: ', energyLevel, 'lycka: ', happinessLevel, 'hunger: ', hungerLevel)
+ }, 400);
+}
+
+/*   const feedButton = document.querySelector('.tamagotchi__button tamagotchi_button--eat')
+    feedButton.addEventListener('click', function() {
+    feedTamagotchi()})
+
+ function feedTamagotchi(){ 
+
+        if (hungerLevel < 0) {
+        hungerLevel += 10;
+            if (hungerLevel <= 100) {
+                hungerLevel = 100;
+                console.log("Bikkjen is full!");
+            }
+        }
+}
+ */
 
 function sleepTamagotchi(){
 
@@ -23,5 +93,4 @@ function changeTamagotchiColor(){
 }
 
 function resetTamagotchi(){
-
 }
